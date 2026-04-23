d2bc46c8e276a89981e348888d604f0dc3e02998
Monica Sargent <monicasargentp@gmail.com>
2026-04-23 11:18:38 +0100

    Connected PHP Database with a mySQL database  and established connections

--- index.php
+++ index.php
@@ -1,6 +1,25 @@
 <?php
+    // Connection to the PHP Database within brighton domains
+    Class Database {
+        private $servername = "brighton";
+        private $username = "msp53_test";
+        private $password = "Str0ngPassword!";
+        private $dbname = "msp53_ci527-assign2";
+        public $conn;
 
-
+        public function connect() {
+            $this->conn = new mysqli(
+                $this->servername, 
+                $this->username, 
+                $this->password, 
+                $this->dbname);
+            
+            if ($this->conn->connect_error) {
+                die("Connection failed: " . $this->conn->connect_error);
+            }
+        }
+    }
 
 
 ?>
+<title>CI527 - API Server</title>
